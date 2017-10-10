4017412261ca6b4030f6af1141a35312137e0a2d
aucan <aucan@AUCAN>
2017-10-10 16:35:42 +0300

    naivebayes

---
+++ ClassifyTextwBOW/Main.py
@@ -0,0 +1,18 @@
+from NaiveBayes import  Pool
+import os
+
+DClasses = ["clinton",  "lawyer",  "math",  "medical",  "music",  "sex"]
+
+base = "learn/"
+p = Pool()
+for i in DClasses:
+    p.learn(base + i, i)
+
+
+
+base = "test/"
+for i in DClasses:
+    dir = os.listdir(base + i)
+    for file in dir:
+        res = p.Probability(base + i + "/" + file)
+        print(i + ": " + file + ": " + str(res))
